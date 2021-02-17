528d3d787d9623c37c64e9fb0f5a5ba397b73a54
Enzo Cranford <enzo@cranford.fr>
2021-02-17 11:19:08 +0100

    Add tom-select js to app.js
    Also rename styles.scss to main.scss in app.js require

--- src/app.js
+++ src/app.js
@@ -1,1 +1,35 @@
-require('../assets/stylesheets/style.scss')
+require('../assets/stylesheets/main.scss');
+
+import TomSelect from 'tom-select';
+
+new TomSelect('#language-select', {
+  create: false,
+  valueField: 'id',
+  items: [1],
+  controlInput: '<input/>',
+  hideSelected: true,
+  options: [
+    { id: 1, option: 'Français', flag: 'fr' },
+    { id: 2, option: 'English', flag: 'gb' },
+  ],
+  render: {
+    option(data, escape) {
+      return (
+        `${
+          '<div class="language-option">' + '<span class="flag-icon flag-icon-'
+        }${escape(data.flag)}"></span>` +
+        `<span>${escape(data.option)}</span>` +
+        `</div>`
+      );
+    },
+    item(data, escape) {
+      return (
+        `${
+          '<div class="language-option">' + '<span class="flag-icon flag-icon-'
+        }${escape(data.flag)}"></span>` +
+        `<span>${escape(data.option)}</span>` +
+        `</div>`
+      );
+    },
+  },
+});
